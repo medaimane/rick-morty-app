a35aedd990ac19c7f5e22e8df7577da9cacc255d
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2021-11-07 04:09:36 +0100

    Add SafeAreaProvider as wrapper

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import {Provider} from 'react-redux';
 import store from './src/store/store';
 import {Navigation} from './src/navigation/Navigation';
 import {Container} from './src/components/Container';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Container>
-        <Navigation />
-      </Container>
-      <StatusBar style="dark" />
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <Container>
+          <Navigation />
+        </Container>
+        <StatusBar style="dark" />
+      </Provider>
+    </SafeAreaProvider>
   );
 }
